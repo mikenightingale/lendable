704666c7c93a49d8bf63d14fe03f37e5b61a86c3
Michael Nightingale <michaeln.public@icloud.com>
2024-09-05 16:37:49 +0100

    fix: build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.3.3"
+	java
+	kotlin("jvm") version "2.0.10"
+	kotlin("plugin.spring") version "1.9.24"
+	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
 }
 
@@ -29,6 +30,7 @@ dependencies {
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
 
 kotlin {
 	compilerOptions {
